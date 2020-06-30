e38e095262e05dd44effe1d3d8aebd7d51f99a4e
Pavlo Dumyak <pavlo.dumyak@kindgeek.com>
2020-06-30 21:40:39 +0300

    Initial Commit

---
+++ PlastovaProba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlastovaProba
+//
+//  Created by Pavlo Dumyak on 30.06.2020.
+//  Copyright © 2020 Pavlo Dumyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
