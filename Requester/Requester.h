c1a815905fd8cbf04e66c44201cf71d261d5b237
Pavlo Dumyak <pavlo.dumyak@kindgeek.com>
2020-11-20 16:07:11 +0200

    Initial Commit

---
+++ Requester/Requester.h
@@ -0,0 +1,18 @@
+//
+//  Requester.h
+//  Requester
+//
+//  Created by Pavlo Dumyak on 20.11.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Requester.
+FOUNDATION_EXPORT double RequesterVersionNumber;
+
+//! Project version string for Requester.
+FOUNDATION_EXPORT const unsigned char RequesterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Requester/PublicHeader.h>
+
+
