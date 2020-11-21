113a8a872dc668aa0a321da7587ce5c5b81eea35
Pavlo Dumyak <pavlo.dumyak@kindgeek.com>
2020-11-21 21:45:58 +0200

    Fixed an error

--- Requesto.podspec
+++ Requesto.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name         = 'Requesto'
-  spec.version      = '0.0.3'
+  spec.version      = '0.0.4'
   spec.authors      = { 
     'Pavlo Dumiak' => 'pavlo.dumiak@gmail.com'
   }
--- Requesto/Core/ExecutiveRequestHandler.swift
+++ Requesto/Core/ExecutiveRequestHandler.swift
@@ -20,16 +20,9 @@ open class ExecutiveRequestHandler: RequestHandler {
     }
     
     func processRequest<T>(request: Request, error: Error?, type: T.Type) where T : Decodable {
-        var error = error
         let serviceRequest = request.buildURLRequest()
         let semaphore = DispatchSemaphore.init(value: 0)
         let task = URLSession.shared.dataTask(with: serviceRequest) { data, response, networkError in
-            if request.canceled {
-                print("request cancelled: \(request)")
-            } else if let networkError = networkError {
-                error = networkError
-            }
-            
             let result = RequestParser().parse(object: data, objectType: type, response: response)
             switch result {
             case .success:
